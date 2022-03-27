54a43f21e8a26ba2eef9bd90ec79d35d2a5dfb82
ahzary <ahmed.nazar@rocketmail.com>
2022-03-27 19:18:01 +0200

    move commander

---
+++ src/aubo_control/src/move_plan.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import rospy
+import RPi.GPIO as GPIO
+from aubo_control.msg import armCmd
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+
+
+emg_1_input = 
+
--- src/aubo_control/src/rasp_control.py
+++ src/aubo_control/src/rasp_control.py
@@ -2,18 +2,30 @@
 import rospy
 import RPi.GPIO as GPIO
 from aubo_control.msg import armCmd
-
+from time import sleep
 GPIO.setmode(GPIO.BCM)
 LED = 24
-GPIO.setup(LED, GPIO.OUT)
+stepper = 21
+direction = 20
+GPIO.setup(stepper, GPIO.OUT) 
+GPIO.setup(direction, GPIO.OUT) 
+prev_angle = 0
+    
+def step(data):
+    GPIO.output(stepper,True)
+    GPIO.output(stepper,False)
+    GPIO.output(direction,data)
+    sleep(0.0006)
 
 def armCmd_callback(data):
-    if data.angle[0] > 1.14 :
-        GPIO.output(LED,True)
-    else:
-        GPIO.output(LED,False)
-
-
+    global prev_angle
+    if prev_angle < data.angle[0]:
+        step(True)
+    if prev_angle > data.angle[0]:
+        step(False)
+    prev_angle = data.angle[0]
+    print(prev_angle)
+   
 def listner():
     rospy.init_node('pi_4',anonymous=False)
     rospy.Subscriber("arduino/armCmd",armCmd,armCmd_callback)
