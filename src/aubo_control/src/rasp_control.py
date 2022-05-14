bb18fb2a2ae8e0b1ca444d0ae002dbfaef4ad07b
ahzary <ahmed.nazar@rocketmail.com>
2022-05-14 05:16:25 +0200

    rasp_control modified

--- src/aubo_control/src/rasp_control.py
+++ src/aubo_control/src/rasp_control.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+from numpy import angle
 import rospy
 import RPi.GPIO as GPIO
 from aubo_control.msg import armCmd
 from time import sleep
 GPIO.setmode(GPIO.BCM)
+
+# Notes area:
+
+
+
 LED = 24
 stepper = [21,5,19]
 direction = [20,6,26]
@@ -11,34 +17,65 @@ GPIO.setup(stepper, GPIO.OUT)
 GPIO.setup(direction, GPIO.OUT)
 prev_angle = [0,0,0]
 
+#stepper settings
+angle_deg = 1.8
+angle_rad = angle_deg * 0.01745329251
+step_mode = 1 # 1 for fullstep, 2 for half , etc 
+step_count = [0,0,0]
+step_position = [0,0,0]
+tolerance = angle_rad * 3
+for i in range (3):
+    step_position[i] = step_count[i] * step_mode * angle_rad
 
 def step(i,j,k):
 
+    if i == 1:
+        GPIO.output(direction[0],True)
+    elif i == -1:
+        GPIO.output(direction[0],False)
+
+    if j == 1:
+        GPIO.output(direction[1],True)
+    elif j == -1:
+        GPIO.output(direction[1],False)
+
+    if k == 1:
+        GPIO.output(direction[2],True)
+    elif k == -1:
+        GPIO.output(direction[2],False)
+    
     for h in range(3):
         GPIO.output(stepper[h],True)
         GPIO.output(stepper[h],False)
-        GPIO.output(direction[h],i)
-    sleep(0.0006)
+
+    step_count[0] += i
+    step_count[1] += j
+    step_count[2] += k
+
+   # sleep(0.0006)
     
 
 def armCmd_callback(data):
     global prev_angle
 
-    if prev_angle[0] < data.angle[0] : # for angle 1
-        i=True
+    if  data.angle[0] > step_position[0] and (data.angle[0] - step_position[0]) > tolerance  : # for angle 1
+        i=1
     else:
-        i=False
-    if prev_angle[1] < data.angle[1] : # for angle 2
-        j=True
+        i=-1
+
+    if data.angle[1] < step_position[1] and (data.angle[1] - step_position[1]) > tolerance :     # for angle 2
+        j=1
     else:
-        j=False
-    if prev_angle[2] < data.angle[2] : # for angle 3
-        k=True
+        j=-1
+
+    if prev_angle[2] < data.angle[2] and (data.angle[2] - step_position[2]) > tolerance :     # for angle 3
+        k=1
     else:
-        k=False
+        k=-1
+
     step(i,j,k)
     prev_angle = data.angle
-    print(prev_angle)
+    print(step_count)
  
 
 def listner():
